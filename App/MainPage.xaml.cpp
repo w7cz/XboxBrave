e4475ad23a1fec6bd84e714b9e41b16fa13d7272
w5cz_ <ggkufw@gmail.com>
2026-08-20 22:36:31 +0300

    Implement MainPage class with event handlers

---
+++ App/MainPage.xaml.cpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "MainPage.xaml.g.h"
+#include "AdblockEngine.h"
+
+namespace winrt::XboxBrave::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        void Browser_CoreWebView2Initialized(
+            Microsoft::UI::Xaml::Controls::WebView2 const& sender,
+            Microsoft::UI::Xaml::Controls::CoreWebView2InitializedEventArgs const& args);
+
+        void AddressBar_KeyDown(
+            Windows::Foundation::IInspectable const& sender,
+            Windows::UI::Xaml::Input::KeyRoutedEventArgs const& e);
+
+    private:
+        void* m_adblockEngine{ nullptr };
+        int m_blockedCount{ 0 };
+        void LoadFilterLists();
+        void Navigate(winrt::hstring const& url);
+    };
+}
+
+namespace winrt::XboxBrave::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage> {};
+}
