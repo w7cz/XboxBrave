f55b435642cbd42f13bdd0c0aef0f8e07d51787e
w5cz_ <ggkufw@gmail.com>
2026-08-20 22:34:38 +0300

    Implement App class with launch handling

---
+++ App/App.xaml.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+#include "App.xaml.h"
+#include "MainPage.xaml.h"
+
+using namespace winrt;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Navigation;
+
+namespace winrt::XboxBrave::implementation
+{
+    App::App()
+    {
+        InitializeComponent();
+    }
+
+    void App::OnLaunched(LaunchActivatedEventArgs const&)
+    {
+        Frame rootFrame{ nullptr };
+        auto content = Window::Current().Content();
+        if (content)
+        {
+            rootFrame = content.try_as<Frame>();
+        }
+
+        if (!rootFrame)
+        {
+            rootFrame = Frame();
+            rootFrame.NavigationFailed({ this, [](IInspectable const&, NavigationFailedEventArgs const& e) {
+                throw hresult_error(E_FAIL, hstring(L"Failed to load Page ") + e.SourcePageType().Name);
+            } });
+
+            Window::Current().Content(rootFrame);
+        }
+
+        if (rootFrame.Content() == nullptr)
+        {
+            rootFrame.Navigate(xaml_typename<XboxBrave::MainPage>());
+        }
+
+        Window::Current().Activate();
+    }
+}
