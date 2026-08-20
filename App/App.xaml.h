f5c97b0f72193790b7f62ed9f0b2736d3829cad6
w5cz_ <ggkufw@gmail.com>
2026-08-20 22:33:24 +0300

    Add App class definition in App.xaml.h

---
+++ App/App.xaml.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::XboxBrave::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
+    };
+}
