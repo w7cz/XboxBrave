e8acdda91a195fb181000b395e5412e078c92200
w5cz_ <ggkufw@gmail.com>
2026-08-22 21:09:25 +0300

    Refactor engine_new to use new_with_list_text

--- native/adblock_engine/src/lib.rs
+++ native/adblock_engine/src/lib.rs
@@ -1,14 +1,13 @@
 #![allow(non_snake_case)]
 
 use adblock::Engine;
-use std::ffi::{CStr, CString, c_char};
+use adblock::request::Request;
+use std::ffi::{CStr, c_char};
 
 #[no_mangle]
 pub extern "C" fn engine_new(rules_ptr: *const c_char) -> *mut Engine {
     let rules = unsafe { CStr::from_ptr(rules_ptr) }.to_string_lossy();
-    let filter_set = adblock::lists::FilterSet::new(false);
-    let mut engine = Engine::from_filter_set(filter_set, true);
-    engine.update_filters(vec![rules.to_string()], vec![]);
+    let engine = Engine::new_with_list_text(rules.to_string());
     Box::into_raw(Box::new(engine))
 }
 
@@ -21,9 +20,11 @@ pub extern "C" fn engine_match(
     let engine = unsafe { &*engine };
     let url = unsafe { CStr::from_ptr(url) }.to_string_lossy();
     let source = unsafe { CStr::from_ptr(source) }.to_string_lossy();
-    engine
-        .check_network_urls(&url, &source, "")
-        .matched
+
+    match Request::new(&url, &source, "document") {
+        Ok(request) => engine.check_network_request(&request).matched,
+        Err(_) => false,
+    }
 }
 
 #[no_mangle]
