2de89b03746fdd71f44ab8d763092a4667df9bc7
w5cz_ <ggkufw@gmail.com>
2026-08-22 20:41:02 +0300

    Refactor MainPage implementation and add LoadFilterLists

--- App/MainPage.xaml.cpp
+++ App/MainPage.xaml.cpp
@@ -1,31 +1,79 @@
 #include "pch.h"
-#pragma once
-#include "MainPage.xaml.g.h"
-#include "AdblockEngine.h"
+#include "MainPage.xaml.h"
+#if __has_include("MainPage.g.cpp")
+#include "MainPage.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Input;
+using namespace Microsoft::UI::Xaml::Controls;
 
 namespace winrt::XboxBrave::implementation
 {
-    struct MainPage : MainPageT<MainPage>
+    MainPage::MainPage()
     {
-        MainPage();
+        InitializeComponent();
+        LoadFilterLists();
+    }
 
-        void Browser_CoreWebView2Initialized(
-            Microsoft::UI::Xaml::Controls::WebView2 const& sender,
-            Microsoft::UI::Xaml::Controls::CoreWebView2InitializedEventArgs const& args);
+    MainPage::~MainPage()
+    {
+        if (m_adblockEngine)
+        {
+            engine_free(m_adblockEngine);
+            m_adblockEngine = nullptr;
+        }
+    }
 
-        void AddressBar_KeyDown(
-            Windows::Foundation::IInspectable const& sender,
-            Windows::UI::Xaml::Input::KeyRoutedEventArgs const& e);
+    void MainPage::LoadFilterLists()
+    {
+        const char* rules =
+            "||doubleclick.net^\n"
+            "||googlesyndication.com^\n"
+            "||googleadservices.com^\n"
+            "||adservice.google.com^\n";
 
-    private:
-        void* m_adblockEngine{ nullptr };
-        int m_blockedCount{ 0 };
-        void LoadFilterLists();
-        void Navigate(winrt::hstring const& url);
-    };
-}
+        m_adblockEngine = engine_new(rules);
+    }
 
-namespace winrt::XboxBrave::factory_implementation
-{
-    struct MainPage : MainPageT<MainPage, implementation::MainPage> {};
+    void MainPage::Browser_CoreWebView2Initialized(
+        Microsoft::UI::Xaml::Controls::WebView2 const& /*sender*/,
+        Microsoft::UI::Xaml::Controls::CoreWebView2InitializedEventArgs const& /*args*/)
+    {
+        // مكان مستقبلي: ربط WebResourceRequested بمحرك adblock
+    }
+
+    void MainPage::AddressBar_KeyDown(
+        Windows::Foundation::IInspectable const& /*sender*/,
+        Windows::UI::Xaml::Input::KeyRoutedEventArgs const& e)
+    {
+        if (e.Key() == Windows::System::VirtualKey::Enter)
+        {
+            Navigate(AddressBar().Text());
+        }
+    }
+
+    void MainPage::Navigate(winrt::hstring const& url)
+    {
+        std::wstring input{ url };
+        std::wstring target;
+
+        if (input.find(L"://") != std::wstring::npos)
+        {
+            target = input;
+        }
+        else if (input.find(L'.') != std::wstring::npos &&
+                 input.find(L' ') == std::wstring::npos)
+        {
+            target = L"https://" + input;
+        }
+        else
+        {
+            target = L"https://www.google.com/search?q=" + input;
+        }
+
+        Browser().Source(Windows::Foundation::Uri{ target });
+    }
 }
