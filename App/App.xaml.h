6f3c3858d187e7a1c0cd1230700522b219eecfda
w5cz_ <ggkufw@gmail.com>
2026-09-03 18:00:55 +0300

    Remove factory implementation for App class
    Removed factory implementation for App class.

--- App/App.xaml.h
+++ App/App.xaml.h
@@ -10,10 +10,3 @@ namespace winrt::XboxBrave::implementation
         void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
     };
 }
-
-namespace winrt::XboxBrave::factory_implementation
-{
-    struct App : AppT<App, implementation::App>
-    {
-    };
-}
